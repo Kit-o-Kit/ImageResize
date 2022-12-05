23b6b03347294e3aefb8155b8419548e643bc134
Dean Eichmann <dean.eichmann@gmail.com>
2022-12-05 10:28:17 +0100

    added js file and everything ready for lambda function

--- skript.sh
+++ skript.sh
@@ -2,4 +2,5 @@
 aws s3 mb s3://bildverkleinerungs-upload
 aws s3 mb s3://bildverkleinerungs-output
 
-aws lambda 
+zip bildverkleinern.zip index.js
+aws lambda create-function --function-name bildverkleinern --zip-file fileb://bildverkleinern.zip --handler index.handler --runtime nodejs16.x --role arn:aws:iam::490096896432:role/LabRole
