233f5bc6ee21dadd9a7b31e022a52409823686e6
Dean Eichmann <dean.eichmann@gmail.com>
2022-12-23 19:10:48 +0100

    Zusammen Arbeit

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-aws s3 rb s3://bildverkleinerungs-upload --force 
-aws s3 rb s3://bildverkleinerungs-output --force
-aws lambda delete-function --function-name bildverkleinern
-sudo rm output.json
