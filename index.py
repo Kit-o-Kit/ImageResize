79d0393548fb70175d9ce40c452471e43a2d0f2d
julia.huettenmoser <julia.huettenmoser@ksb-sg.ch>
2022-12-23 20:24:08 +0100

    python skript nicht automatisch

--- index.py
+++ index.py
@@ -1,15 +1,28 @@
+import json
 from PIL import Image
+import boto3
+import logging
+from botocore.exceptions import ClientError
+import os
 
-#define image path
-image = Image.open(r"C:\Users\julia\Downloads\windows_xp_bliss-wide.jpg")
-#define image size
-MAX_SIZE = (100, 100)
+s3Client = boto3.client('s3')
 
-#resize the image
-image.thumbnail(MAX_SIZE)
+def lambda_handler(event, context):
+      bucket = event["records"]["0"]["s3"]["bucket"]["name"]
+      bucket = event["records"]["0"]["s3"]["object"]["key"]
+      
+      print(bucket)
+      print(key)
+      
+      response = s3Client.get_object(Bucket=bucket, Key=key)
+      
+      image = Image.open(lambda_handler)
+      #define image size
+      MAX_SIZE = (100, 100)
+      #resize the image
+      image.thumbnail(MAX_SIZE)
+      #save the image as png. can be changed to other file type.
+      image.save('resizedImage.png')
 
-#save the image as png. can be changed to other file type.
-image.save('resizedImage.png')
-
-#open the image after it got resized
-#image.show()
+      bucket = s3.Bucket('s3://sowwyuwu')
+      bucket.copy(copy_source, '/resizedImage.png')
